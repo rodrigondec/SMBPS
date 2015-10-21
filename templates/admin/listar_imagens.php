d17f276a3ab37da91ff54098471750637ef46e51
Rodrigo Castro <rodrigondec@gmail.com>
2015-10-21 19:19:20 -0300

    modal imagem identado

--- templates/admin/listar_imagens.php
+++ templates/admin/listar_imagens.php
@@ -27,22 +27,20 @@
 				</a>
 				<!-- Modal -->
 				<div id="myModal" class="modal fade" role="dialog">
-				  <div class="modal-dialog modal-lg">
-
-				    <!-- Modal content-->
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        <button type="button" class="close" data-dismiss="modal">&times;</button>
-				        <h4 class="modal-title text-left">Imagem</h4>
-				      </div>
-				      <div class="modal-body text-center">
-				        <img src="<?php echo IMGS.$imagens[$key]['nome']; ?>" width="100%">
-				      </div>
-				      <div class="modal-footer">
-				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				      </div>
-				    </div>
-
+			  		<div class="modal-dialog modal-lg">
+					    <!-- Modal content-->
+					    <div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+								<h4 class="modal-title text-left">Imagem</h4>
+							</div>
+							<div class="modal-body text-center">
+								<img src="<?php echo IMGS.$imagens[$key]['nome']; ?>" width="100%">
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+							</div>
+					    </div>
 				  </div>
 				</div>
 			</td>
