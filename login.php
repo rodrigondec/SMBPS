07a0d40a84b36fbf21a392831fdec35d69180cf7
Rodrigo Castro <rodrigondec@gmail.com>
2015-10-21 19:18:39 -0300

    login feito com modal ao invés de uma página.

--- login.php
+++ login.php
@@ -1,18 +1,3 @@
-<div class='text-center' style='margin-top:5%; margin-bottom:2%;'>
-	<h2>Entrar</h2>
-</div>
-<div class='form-boot-40'>
-	<form action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
-		<div class='form-group'>
-		<input type='email' name='email' class='form-control' id='input_email' placeholder='Email'  required />
-		<input type='password' name='senha' class='form-control' id='input_senha' placeholder='Senha' style='margin-top: 10px;' required />
-		</div>
-		<div id='buttons' class='text-center'>
-			<button type='submit' class='btn btn-default'>Entrar</button>
-		</div>
-	</form>
-</div>
-
 <?php 
 	if(count($_POST) > 0){
 		$usuario = select('*', 'usuario', 'email', $_POST['email'], LINK);
--- templates/geral/menu_sistema.php
+++ templates/geral/menu_sistema.php
@@ -17,8 +17,38 @@
                 <li><a href="/smbps/index.php/sistema/contato">Contato</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/smbps/index.php/login">Entrar</a></li>
+                <li>
+                    <a data-toggle="modal"  data-target="#myModal">
+                        Entrar
+                    </a>
+                </li>
             </ul>
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title text-left">Login</h4>
+            </div>
+            <div class="modal-body text-center">
+                <form action="/smbps/index.php/login" method='post'>
+                    <div class='form-group'>
+                    <input type='email' name='email' class='form-control' id='input_email' placeholder='Email'  required />
+                    <input type='password' name='senha' class='form-control' id='input_senha' placeholder='Senha' style='margin-top: 10px;' required />
+                </div>
+                <div id='buttons' class='text-right'>
+                    <button type='submit' class='btn btn-info'>Entrar</button>
+                </div>
+            </form>
+            </div>
+<!--             <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div> -->
+        </div>
+    </div>
+</div>
