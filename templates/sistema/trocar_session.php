836ebf7239636d044937f23c477513355d056859
Rodrigo Castro <rodrigondec@gmail.com>
2015-10-21 13:55:07 -0300

    trocar_sessao() adicionado

---
+++ templates/sistema/trocar_session.php
@@ -0,0 +1,28 @@
+<?php 
+    if(count($_GET)>0 && isset($_GET['type'])){
+
+    	echo 'GET: ';var_dump($_GET);echo '<br /><br />';
+
+    	session_destroy();
+    	session_start();
+    	if($_GET['type'] == '1'){
+    		// hospital para admin
+
+    		$usuario = select('*', 'usuario', 'email', 'admin@admin.com', LINK);
+    		$_SESSION['email'] = $usuario['email'];
+			$_SESSION['privilegio'] = $usuario['id_papel'];
+    	}
+    	else if($_GET['type'] == '2'){
+    		// admin para hospital
+
+    		$usuario = select('*', 'usuario', 'email', 'gestor@hospital.com', LINK);
+    		$_SESSION['email'] = $usuario['email'];
+			$_SESSION['privilegio'] = $usuario['id_papel'];
+			$_SESSION['hospital'] = $usuario['id_hospital'];
+    	}
+
+    	echo 'session: ';var_dump($_SESSION);echo '<br /><br />';
+    }
+    ob_clean();
+	header('LOCATION: /smbps/');
+?>
