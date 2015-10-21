73d4c3e5700f10d4aa05a9d88d6cecf6ee41a1ac
Rodrigo Castro <rodrigondec@gmail.com>
2015-10-21 13:53:35 -0300

    minor fix no login

--- login.php
+++ login.php
@@ -1,22 +1,23 @@
-<div align='center' style='margin-top:5%; margin-bottom:2%;'>
-	<h2>Login</h2>
+<div class='text-center' style='margin-top:5%; margin-bottom:2%;'>
+	<h2>Entrar</h2>
 </div>
 <div class='form-boot-40'>
 	<form action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
-			<div class='form-group'>
-			<input type='email' name='email' class='form-control' id='input_email' placeholder='Email'  required />
-			<input type='password' name='senha' class='form-control' id='input_senha' placeholder='Senha' style='margin-top: 10px;' required />
-			</div>
-			<div id='buttons' class='text-center'>
-				<button type='submit' class='btn btn-default'>Entrar</button>
-			</div>
-		</form>
-	</div>
+		<div class='form-group'>
+		<input type='email' name='email' class='form-control' id='input_email' placeholder='Email'  required />
+		<input type='password' name='senha' class='form-control' id='input_senha' placeholder='Senha' style='margin-top: 10px;' required />
+		</div>
+		<div id='buttons' class='text-center'>
+			<button type='submit' class='btn btn-default'>Entrar</button>
+		</div>
+	</form>
+</div>
 
 <?php 
 	if(count($_POST) > 0){
 		$usuario = select('*', 'usuario', 'email', $_POST['email'], LINK);
 		if($usuario && $usuario['senha'] == md5($_POST['senha'])){
+			session_start();
 			var_dump($usuario);
 			$_SESSION['email'] = $usuario['email'];
 			$_SESSION['privilegio'] = $usuario['id_papel'];
