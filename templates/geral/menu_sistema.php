8cb4a2584b4340e8dc407c9230da0402c0103cda
Rodrigo Castro <rodrigondec@gmail.com>
2015-10-21 21:29:21 -0300

    fix pagina inicial menu sistema

--- templates/geral/menu_sistema.php
+++ templates/geral/menu_sistema.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo BASE; ?>">Página inicial</a>
+            <a class="navbar-brand" href="/<?php echo BASE; ?>">Página inicial</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
