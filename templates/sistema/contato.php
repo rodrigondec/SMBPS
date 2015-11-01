6299e7b5391611dd150448f6bdc79daa3f91fee3
Rodrigo Castro <rodrigondec@gmail.com>
2015-11-01 15:58:05 -0300

    adicionado contato do sistema

--- templates/sistema/contato.php
+++ templates/sistema/contato.php
@@ -1,1 +1,28 @@
-contato sistema
+<div class='text-center'>
+	<h2>Contato</h2>
+	<hr />
+</div>
+<form class='centered-30' method='post'>
+	<div class='form-group text-left'>
+	    <label for='nome'>Nome</label>
+	    <input type='nome' name='nome' class='form-control' placeholder='Nome'  required />
+	</div>
+	<div class='form-group text-left'>
+	    <label for='email'>Email</label>
+	    <input type='email' name='email' class='form-control' placeholder='Email'  required />
+	</div>
+	<div class='form-group text-left'>
+	    <label for='comentario'>Comentário</label>
+	    <textarea rows='5' name="comentario" class='form-control' required></textarea>
+
+	</div>
+	<div class='text-center'>
+		<input type='reset' value='Apagar' class='btn btn-warning' />
+		<input type='submit' value='Enviar' class='btn btn-primary' />
+	</div>
+</form>
+<?php 
+    if(count($_POST) > 0){
+    	var_dump($_POST);
+    }
+?>
