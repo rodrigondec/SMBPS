ed8c4119ac0813512438e2eb0326f589192fa231
Rodrigo Castro <rodrigondec@gmail.com>
2015-11-01 07:06:29 -0300

    adicionada página do protocolo que servirá para todas as coisas relacionadas aos protocolos e utilizada por todos os indicadores diferentes

---
+++ templates/hospital/protocolo.php
@@ -0,0 +1,15 @@
+<?php 
+    $indicador = select('*', 'indicador', 'id', $_GET['id']);
+    $protocolo = select('*', 'protocolo', '(id_hospital, id_indicador, ativo)', '('.$_SESSION['hospital'].', '.$_GET['id'].', \'1\')', false);
+?>
+<div class='text-center'><h2><?php echo $indicador['nome']; ?></h2></div>
+<?php 
+	if(!$protocolo){
+		echo "<div class='text-center text-danger'><h3>Seu hospital não possui esse protocolo. Favor cadastra-lo.</h3></div>";
+		include_once('cadastrar_protocolo.php');
+	}
+	else{
+		echo "<div class='text-center text-warning'><h3>Esse protocolo do seu hospital já é muito antigo. Favor cadastra-lo novamente.</h3></div>";
+		include_once('alterar_protocolo.php');
+	}
+?>
