8a75a781926d6f6ff7f7e645b3b115528cce319b
Rodrigo Castro <rodrigondec@gmail.com>
2015-11-24 18:17:19 -0300

    alterar usuário não altera tipo de usuário/papel

--- templates/admin/listar_usuarios.php
+++ templates/admin/listar_usuarios.php
@@ -28,10 +28,14 @@
 				<?php echo $usuarios[$key]['id']; ?>
 			</td>
 			<td class='col-md-1'>
-				<?php echo $usuarios[$key]['id_papel']; ?>
+				<?php 
+					echo $usuarios[$key]['id_papel']; 
+				?>
 			</td>
 			<td class='col-md-1'>
-				<?php echo $usuarios[$key]['id_hospital']; ?>
+				<?php 
+					echo $usuarios[$key]['id_hospital']; 
+				?>
 			</td>
 			<td>
 				<?php echo $usuarios[$key]['nome']; ?>
@@ -53,53 +57,20 @@
 								<h4 class="modal-title">Alterar Usuário</h4>
 							</div>
 							<div class="modal-body">
-								<form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+								<form method='post'>
 									<input type='number' name='id' value="<?php echo $usuarios[$key]['id']; ?>" hidden placeholder='' required />
-									<div class='form-group'>
-                        				<label for='id_papel'>Papel</label>
-										<select class='form-control' name='id_papel' <?php echo "onblur='select_hospital(".$usuarios[$key]['id'].", $(this).val())'";  ?> required>
-											<?php 
-											    foreach ($papeis as $key2 => $value):
-											?>
-											<option value='<?php echo $papeis[$key2]['id']; ?>' <?php if($usuarios[$key]['id_papel'] == $papeis[$key2]['id']){echo 'selected';} ?>>
-												<?php echo $papeis[$key2]['nome']?>
-											</option>
-											<?php
-											    endforeach;
-											?>
-										</select>
-									</div>
-									<div id='div_select_hospital<?php echo $usuarios[$key]['id']; ?>' class='form-group <?php if($usuarios[$key]['id_papel'] == 1){ echo 'hidden'; } ?>'>
-                        				<label for='id_hospital'>Hospital</label>
-										<select id='select_hospital<?php echo $usuarios[$key]['id']; ?>' class='form-control' name='id_hospital' id='select_hospital<?php echo $usuarios[$key]['id']; ?>' required>
-											<option value='' disabled selected>Selecionar hospital para gerir</option>
-											<?php 
-											    foreach ($hospitais as $key2 => $value):
-											?>
-											<option value='<?php echo $hospitais[$key2]['id']; ?>' <?php if($usuarios[$key]['id_hospital'] == $hospitais[$key2]['id']){echo 'selected';} ?>>
-												<?php echo $hospitais[$key2]['nome']?>
-											</option>
-											<?php
-											    endforeach;
-											?>
-										</select>
-									</div>
-
 									<div class='form-group'>
                         				<label for='nome'>Nome</label>
 										<input class='form-control' type='text' name='nome' value="<?php echo $usuarios[$key]['nome']; ?>" placeholder='Nome' required />
 									</div>
-									
 									<div class='form-group'>
                         				<label for='email'>Email</label>
 										<input class='form-control' type='email' name='email' value="<?php echo $usuarios[$key]['email']; ?>"placeholder='Email' required />
 									</div>
-									
 									<div class='form-group'>
                         				<label for='senha'>Senha</label>
 										<input class='form-control' type='password' name='senha' placeholder='senha' />
 									</div>
-									
 									<input type='submit' value='Alterar' class='btn btn-primary' />
 								</form>
 							</div>
@@ -135,16 +106,3 @@
     	header('LOCATION: '.ADMIN.'listar_usuarios');
     }
 ?>
-<script type="text/javascript">
-	function select_hospital(id, valor){
-		console.log(valor);
-		if(valor == 1){
-			$("#div_select_hospital"+id).attr('class', 'form-group hidden')
-			$("#select_hospital"+id).attr('required', false)
-		}
-		else if(valor == 2){
-			$("#div_select_hospital"+id).attr('class', 'form-group')
-			$("#select_hospital"+id).attr('required', true)
-		}
-	}
-</script>
