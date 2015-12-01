d70080e07b16395f9f93720d76388f12f8bcfa50
Rodrigo Castro <rodrigondec@gmail.com>
2015-12-01 20:18:02 -0300

    adicionado pegar cidade por geolocalização. passando functions de javascript para o functions.js

--- estaticos/functions.js
+++ estaticos/functions.js
@@ -40,3 +40,62 @@ function sa(head, body, tipo, loc, btn){
 		});
 	}
 }
+
+function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    codeLatLng(lat, lng)
+}
+
+function codeLatLng(lat, lng) {
+	geocoder = new google.maps.Geocoder();
+  	var latlng = new google.maps.LatLng(lat, lng);
+  	geocoder.geocode({latLng: latlng}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      	if (results[1]) {
+	        	var arrAddress = results;
+	        	$.each(arrAddress, function(i, address_component) {
+	          		if (address_component.types[0] == "locality") {
+	            		$('#cidade').attr('value', address_component.address_components[0].long_name)
+	            		itemLocality = address_component.address_components[0].long_name;
+	          		}
+	        	});
+	      	} 
+	      	else {
+	        	alert("No results found");
+	      	}
+	    } 
+	    else {
+	      	alert("Geocoder failed due to: " + status);
+	    }
+  	});
+}
+
+function pegar_cidade(){
+	var geocoder;
+  	
+	//Get the latitude and the longitude;
+	if (navigator.geolocation){
+    	navigator.geolocation.getCurrentPosition(successFunction);
+	} 
+}
+
+function show_hide_cidade(valor){
+	// console.log(valor);
+	for (id = 1; id <= num_estados; id++){
+		if(id == valor){
+			// HIDE TODOS OS SELECTS
+			for (j = 1; j <= num_estados; j++) {
+				$("#div_select_cidade"+j).attr('class', 'form-group hidden')
+				$("#select_cidade"+j).attr('required', false)
+			}
+			// SHOW SELECIONADO
+			$("#div_select_cidade"+id).attr('class', 'form-group')
+			$("#select_cidade"+id).attr('required', true)
+		}
+		else{
+
+		}
+	}
+}
