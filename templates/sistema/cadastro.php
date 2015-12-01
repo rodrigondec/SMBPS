97b7f8cd7951414b34d11e348bd424d1c4664757
Rodrigo Castro <rodrigondec@gmail.com>
2015-12-01 20:19:50 -0300

    cadastro no sistema funcionando. Falta adicionar a notificação para os admins do sistema com a informação cadastrada

--- templates/sistema/cadastro.php
+++ templates/sistema/cadastro.php
@@ -2,22 +2,254 @@
 	<h2>Cadastro</h2>
 	<hr />
 </div>
-<form class='centered-30' method='post'>
-	<div class='form-group text-left'>
-	    <label for='nome'>Nome</label>
-	    <input type='nome' name='nome' class='form-control' placeholder='Nome'  required />
-	</div>
-	<div class='form-group text-left'>
-	    <label for='email'>Email</label>
-	    <input type='email' name='email' class='form-control' placeholder='Email'  required />
-	</div>
-	<div class='text-center'>
-		<input type='reset' value='Apagar' class='btn btn-warning' />
-		<input type='submit' value='Enviar' class='btn btn-primary' />
-	</div>
-</form>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript">
+	pegar_cidade();
+</script>
 <?php 
     if(count($_POST) > 0){
-    	var_dump($_POST);
-    }
+    	//var_dump($_POST);
+    	try {
+    		$dados = array();
+    		foreach ($_POST as $key => $value) {
+    			if($key == 'nome_usuario' || $key == 'email' || $key == 'senha'){
+    				if($key == 'nome_usuario'){
+    					$key = 'nome';
+    				}
+    				$dados['usuário'][$key] = $value;
+    			}
+    			else{
+    				if($key == 'nome_hospital'){
+    					$key = 'nome';
+    				}
+    				else if($key == 'cidade_str'){
+    					$key = 'id_cidade';
+    					$id_cidade = select('id', 'cidade', 'nome', $value)['id'];echo '<br /><br />';
+    				}
+    				$dados['hospital'][$key] = retirar_mascara($key, $value);
+    			}
+	    	}
+
+	    	$dados['usuário']['id_papel'] = select('id', 'papel', 'nome', 'Gestor Hospitalar')['id'];echo '<br /><br />';
+	    	$dados['hospital']['id_cidade'] = $id_cidade;
+
+	    	// var_dump(validar_cnpj($_POST['cnpj']));
+	    	/* CNPJ INVÁLIDO */
+	    	/*if(!validar_cnpj($_POST['cnpj'])){
+	    		throw new Exception('O CNPJ ´'.$_POST['cnpj'].'´ é inválido', 100);
+	    	}*/
+
+	    	/* EMAIL DUPLICADO */
+	    	$usuario = select('*', 'usuário', 'email', $_POST['email']);
+	    	if(count($usuario) > 1){
+	    		throw new Exception('O email ´'.$_POST['email'].'´ já está em uso', 102);
+	    	}
+
+	    	/* CNPJ DUPLICADO */
+	    	$hospital = select('*', 'hospital', 'cnpj', $_POST['cnpj']);
+	    	if(count($hospital) > 1){
+	    		throw new Exception('O CNPJ ´'.$_POST['cnpj'].'´ já está em uso', 101);
+	    	}
+
+	    	/* MYSQL INSERT USUÁRIO E HOSPITAL */
+	    	foreach ($dados as $key => $value) {
+	    		if(!insert($dados[$key], $key)){
+	    			throw new Exception(mysql_error(LINK), 113);
+	    		}
+	    	}
+
+	    	$cnpj = $dados['hospital']['cnpj'];
+	    	$email = $dados['usuário']['email'];
+
+	    	unset($dados);
+	    	$dados = array();
+	    	$dados['id_hospital'] = select('id', 'hospital', 'cnpj', $cnpj)['id'];
+	    	/* MYSQL UPDATE ID ACESSO HOSPITAL DO USUÁRIO */
+	    	if(!update($dados, 'usuário', 'email', $email)){
+    			throw new Exception(mysql_error(LINK), 113);
+    		}
+
+
+
+
+
+	    	/*ob_clean();
+    		header('LOCATION: /'.BASE);*/
+    	} catch (Exception $e){
+    		/* CNPJ INVÁLIDO */
+    		if($e->getCode() == 100){
+    			$titulo = 'CNPJ inválido!';
+    			$mensagem = $e->getMessage();
+    		}
+    		/* CNPJ DUPLICADO */
+    		else if($e->getCode() == 101){
+    			$titulo = 'CNPJ duplicado!';
+	    		$mensagem = $e->getMessage();
+	    	}
+	    	/* EMAIL DUPLICADO */
+    		else if($e->getCode() == 102){
+    			$titulo = 'Email duplicado!';
+	    		$mensagem = $e->getMessage();
+	    	}
+    		/* MYSQL ERROR INSERT */
+    		else if($e->getCode() == 113){
+    			$titulo = 'Erro ao inserir no banco de dados!';
+	    		$mensagem = str_replace('\'', '´', $e->getMessage());
+	    	}
+	    	echo '<script type="text/javascript">var titulo = \''.$titulo.'\';</script>';
+	    	echo '<script type="text/javascript">var mensagem = \''.$mensagem.'\';</script>';
 ?>
+<script type="text/javascript">
+	window.onload = function(){
+		if(titulo == 'CNPJ inválido!' || titulo == 'CNPJ duplicado!'){
+			swal({
+				title: titulo,
+				text: mensagem,
+				type: 'error',
+				confirmButtonClass: 'btn-danger',
+				html: false
+			}, 
+			function(){
+				$('#erro_cnpj').attr('class', 'alert alert-danger alert-dismissible')
+				$('#titulo_erro_cnpj').html(titulo)
+			});
+		}
+		else if(titulo == 'Email duplicado!'){
+			swal({
+				title: titulo,
+				text: mensagem,
+				type: 'error',
+				confirmButtonClass: 'btn-danger',
+				html: false
+			}, 
+			function(){
+				$('#erro_email').attr('class', 'alert alert-danger alert-dismissible')
+				$('#titulo_erro_email').html(titulo)
+			});
+		}
+		else if(titulo == 'Erro ao inserir no banco de dados!'){
+			swal({
+				title: titulo,
+				text: mensagem,
+				type: 'error',
+				confirmButtonClass: 'btn-danger',
+				html: false
+			});
+		}
+	}
+</script>
+<?php
+    	}
+    }
+	$estados = select_many('id, nome, uf', 'estado');
+	foreach ($estados as $key => $estado){
+		$estados[$key]['cidades'] = select_many('id, nome', 'cidade', 'id_estado', $estado['id']);
+	}
+	echo '<script type="text/javascript">var num_estados = '.count($estados).';</script>';
+?>
+<div class='container col-md-6 col-lg-6 col-sm-6 col-xs-7 center' method='post'>
+	<form method='post'>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+			    <h3 class="panel-title">Informações do Usuário</h3>
+			</div>
+			<div class="panel-body">
+			    <div class='form-group'>
+				    <label for='nome'>Nome</label>
+				    <input type='text' name='nome_usuario' class='form-control' placeholder='Nome' value='<?php if(count($_POST) > 0){echo $_POST['nome_usuario'];} ?>'  required />
+				</div>
+				<div class='form-group'>
+					<div id='erro_email' class="alert alert-danger alert-dismissible hidden" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<div><strong id='titulo_erro_email'></strong></div>Verifique o email e tente novamente.
+					</div>
+				    <label for='email'>Email</label>
+				    <input type='email' name='email' class='form-control' placeholder='Email' value='<?php if(count($_POST) > 0){echo $_POST['email'];} ?>' required />
+				</div>
+				<div class='form-group'>
+				    <label for='senha'>Senha</label>
+				    <input type='password' name='senha' class='form-control' placeholder='Senha' value='<?php if(count($_POST) > 0){echo $_POST['senha'];} ?>' required />
+				</div>
+			</div>
+		</div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+			    <h3 class="panel-title">Informações do Hospital</h3>
+			</div>
+			<div class="panel-body">
+			    <div class='form-group'>
+				    <label for='cidade'>Cidade</label>
+				    <input type='text' name='cidade_str' id='cidade' class='form-control' readonly required />
+				</div>
+				<div class='form-group'>
+					<label for='nome'>Nome</label>
+					<input class='form-control' type='text' name='nome_hospital' placeholder='Nome' value='<?php if(count($_POST) > 0){echo $_POST['nome_hospital'];} ?>' required />
+				</div>
+				<div class='form-group hidden'>
+					<label for='select_estado'>Estado</label>
+					<div onclick='show_hide_cidade($("#select_estado").selectpicker("val"))'>
+					<select id='select_estado' class='form-control selectpicker' data-style="btn-info" data-live-search='true'>
+						<option disabled selected value=''>Selecione um estado</option>
+						<?php 
+						    foreach ($estados as $key => $estado):
+						?>
+						<option value='<?php echo $estado['id']; ?>'>
+							<?php echo $estado['nome']?>
+						</option>
+						<?php
+						    endforeach;
+						?>
+					</select>
+					</div>
+				</div>
+				<?php
+				    foreach ($estados as $key => $estado):
+				?>
+				<div id='div_select_cidade<?php echo $estado['id']; ?>' class='form-group hidden'>
+					<label for='id_cidade'>Cidade (<?php echo $estado['nome'] ?>)</label>
+					<select id='select_cidade<?php echo $estado['id']; ?>' class='form-control selectpicker' data-style="btn-info" data-live-search='true' name='id_cidade'>
+						<option value='' disabled selected>Selecione uma cidade</option>
+						<?php 
+							foreach ($estado['cidades'] as $key2 => $cidade):
+						?>
+						<option value='<?php echo $cidade['id']; ?>'>
+							<?php echo $cidade['nome']; ?>
+						</option>
+						<?php endforeach; ?>
+					</select>
+				</div>
+				<?php 
+				    endforeach;
+				?>
+				<div class='form-group'>
+					<label for='cnpj'>CNPJ</label>
+					<div id='erro_cnpj' class="alert alert-danger alert-dismissible hidden" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<div><strong id='titulo_erro_cnpj'></strong></div>Verifique o CNPJ e tente novamente.
+					</div>
+					<input class='form-control' type='text' name='cnpj' data-mask='00.000.000/0000-00' placeholder='CNPJ' value='<?php if(count($_POST) > 0){echo $_POST['cnpj'];} ?>' pattern='^[0-9]{2}\.[0-9]{3}\.[0-9]{3}/[0-9]{4}-[0-9]{2}' title='xx.xxx.xxx/xxxx-xx' required />
+				</div>
+				<div class='form-group'>
+					<label for='telefone'>Telefone</label>
+					<input class='form-control' type='text' name='telefone' data-mask='(00) 0000-0000' placeholder='Telefone' value='<?php if(count($_POST) > 0){echo $_POST['telefone'];} ?>' pattern='^\([0-9]{2}\) [0-9]{4}-[0-9]{4}' title='(xx) xxxx-xxxx' required />
+				</div>
+				<div class='form-group'>
+					<label for='endereço'>Endereço</label>
+					<input class='form-control' type='text' name='endereço' placeholder='Endereço' value='<?php if(count($_POST) > 0){echo $_POST['endereço'];} ?>' required />
+				</div>
+				<div class='form-group'>
+					<label for='cep'>CEP</label>
+					<input class='form-control' type='text' name='cep' data-mask='00.000-000' placeholder='CEP' value='<?php if(count($_POST) > 0){echo $_POST['cep'];} ?>' pattern="^[0-9]{2}\.[0-9]{3}-[0-9]{3}" title='xx.xxx-xxx' required />
+				</div>
+			</div>
+		</div>
+		<div class='text-center'>
+			<input type='reset' value='Apagar' class='btn btn-warning' />
+			<input type='submit' value='Enviar' class='btn btn-primary' />
+		</div>
+	</form>
+</div>
