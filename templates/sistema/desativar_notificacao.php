67a8bb1e0d669eb6b058ca2f2887b9333bfb1eb0
Rodrigo Castro <rodrigondec@gmail.com>
2015-12-02 13:26:21 -0300

    desativar_notificação refatorado

--- templates/sistema/desativar_notificacao.php
+++ templates/sistema/desativar_notificacao.php
@@ -3,8 +3,22 @@
     	$dados['ativa'] = 0;
     	var_dump($_POST);echo '<br /><br />';
     	var_dump($dados);
-    	update($dados, 'notificação', 'id', $_POST['id']);
-    	ob_clean();
-    	header('LOCATION: /'.BASE);
+    	try {
+    		if(!update($dados, 'usuário_notificação', '(id_usuário, id_notificação)', '('.$_SESSION['id_usuario'].', '.$_POST['id'].')', false)){
+    			throw new Exception(mysql_error(LINK), 113);
+    		}
+    		ob_clean();
+    		header('LOCATION: /'.BASE);
+    	} catch (Exception $e) {
+    		if($e->getCode() == 113){
+    			$titulo = 'Erro no banco de dados!';
+	    		$mensagem = str_replace('\'', '´', $e->getMessage());
+	    	}
+	    	echo '<script type="text/javascript">var titulo = \''.$titulo.'\';</script>';
+	    	echo '<script type="text/javascript">var mensagem = \''.$mensagem.'\';</script>';
+	    	swal($titulo, $mensagem, 'error', '', 'btn-danger');
+    	}
+    	
+    	
     }
 ?>
