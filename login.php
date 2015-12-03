27626981f772cbc9dcd7c2d3e4d2bd5ccf573c75
Rodrigo Castro <rodrigondec@gmail.com>
2015-12-03 14:11:56 -0300

    login verifica se usuário é ativo

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php 
 	if(count($_POST) > 0){
-		$usuario = select('*', 'usuário', 'email', $_POST['email']);
+		$usuario = select('*', 'usuário', '(email, ativo)', '(\''.$_POST['email'].'\', 1)', false);
 		if($usuario && $usuario['senha'] == md5($_POST['senha'])){
 			session_start();
-			var_dump($usuario);
+			// var_dump($usuario);
 			$_SESSION['id_usuario'] = $usuario['id'];
 			$_SESSION['email'] = $usuario['email'];
 			$_SESSION['privilegio'] = $usuario['id_papel'];
