45aa8b3def5ad3a0ba37eb20d87fd6b8a5dcf7c2
Rodrigo Castro <rodrigondec@gmail.com>
2015-12-03 21:37:37 -0300

    dados protocolo = cadastrar formulário

--- templates/hospital/dados_protocolo.php
+++ templates/hospital/dados_protocolo.php
@@ -1,18 +1,80 @@
 <?php 
     $indicador = select('*', 'indicador', 'id', $_GET['id']);
-    $protocolo = select('*', 'protocolo', '(id_hospital, id_indicador, ativo)', '('.$_SESSION['id_hospital'].', '.$_GET['id'].', \'1\')', false);
 ?>
 <div class='text-center'>
 	<h2>Dados Indicador <?php echo $indicador['nome']; ?></h2>
 	<hr />
 </div>
 <?php 
-	if(!$protocolo){
-		echo "<div class='text-center text-danger'><h3>Seu hospital não possui esse protocolo cadastrado. Favor cadastra-lo.</h3></div>";
-		include_once('cadastrar_protocolo.php');
-	}
-	else{
-		echo "<div class='text-center text-success'><h3>Seu hospital tem esse protocolo!</h3></div>";
-		// include_once('alterar_protocolo.php');
-	}
+    $perguntas = select_many('*', 'pergunta', 'id_indicador', $_GET['id']);
+    // var_dump($perguntas);
+?>
+<div class='container'>
+	<form action='<?php echo $_SERVER['PHP_SELF']?>' method='post'>
+		<p class='obrigatorio'><span class='obrigatorio'>*obrigatorio</span></p>
+
+		<?php 
+		$contador = 1;
+	    foreach ($perguntas as $key => $value):
+	    	$pergunta_inputs = select_many('*', 'pergunta_input', 'id_pergunta', $perguntas[$key]['id']);
+	    	// var_dump($pergunta_inputs);echo '<br /><br />';
+	    	foreach ($pergunta_inputs as $key2 => $value) {
+	    		$inputs[$key2] = select('*', 'input', 'id', $pergunta_inputs[$key2]['id_input']);
+	    	}
+	    	// var_dump($inputs);
+		?>
+		<div class='input'>
+			<p>
+				<?php 
+				    echo $contador.' - '.$perguntas[$key]['texto'];
+				    $contador++;
+				    if($perguntas[$key]['obrigatória'] == '1'){echo " <span class='obrigatorio'>*</span>";}
+				    if($perguntas[$key]['observação'] != ''):
+				?>
+				<h5>
+					<?php 
+				    	echo $perguntas[$key]['observação'];
+				    ?>
+				</h5>
+				<?php endif; ?>
+			</p>
+			<?php 
+			    foreach ($pergunta_inputs as $key2 => $value) {
+			    	echo "<div>";
+
+		    		echo "<input ";
+		    		if($inputs[$key2]['type'] != 'radio'){
+		    			echo "class='form-control'";
+		    		}
+		    		echo "type='".$inputs[$key2]['type']."' ";
+		    		echo "name='".$pergunta_inputs[$key2]["name"]."' ";
+		    		echo "value='".$inputs[$key2]["value"]."'";
+		    		if($perguntas[$key]['obrigatória'] == '1'){
+		    			echo " required";
+		    		}
+		    		echo " />";
+
+		    		if($inputs[$key2]['type'] == 'radio'){
+		    			echo " ".$inputs[$key2]["value"];
+		    		}
+
+		    		echo "</div>";
+		    	}
+			?>
+		</div>
+	
+	<?php
+	    endforeach;
+	?>
+		<div class='text-center'>
+			<input class='btn btn-primary' type='submit' value='Enviar' />
+			<input class='btn btn-danger' type='reset' value='Apagar' />
+		</div>	
+	</form>
+</div>
+
+<?php 
+    if(count($_POST) > 0){
+    	
+    }
 ?>
