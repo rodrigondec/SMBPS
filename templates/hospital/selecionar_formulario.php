57f50591a902097c55fad085802cc14336dc24a8
Rodrigo Castro <rodrigondec@gmail.com>
2016-01-15 17:12:43 -0300

    adicionado selection_formulario.php

--- templates/hospital/selecionar_formulario.php
+++ templates/hospital/selecionar_formulario.php
@@ -9,34 +9,29 @@
     var_dump($meses);
 ?>
 <script type="text/javascript">
-	var neededData; 
 	$(document).ready(function(){
-		$('#select_mes').change(function(){
+		$('#enviar').click(function(){
 			setor = $("#select_setor").selectpicker("val");
 			mes = $("#select_mes").selectpicker("val");
+			console.log(setor);
+			console.log(mes);
 			if(mes != null && setor != null){
-				$.ajax({
-			        type: "GET",
-			        url: 'http://localhost/smbps/templates/hospital/get_formulario.php',
-			        data: {id_mes: mes, id_setor: setor},
-			        dataType: "jsonp",
-			        success: function(data){
-			        	neededData = data;
-			        	console.log(neededData);
-			    	}
-				});	
+				$('#selecter').submit()
 			}
-			else if(mes != null){
+			else if(setor == null){
 				sa('Erro', 'Selecione o setor desejado', 'error', '', 'btn-danger');
-			}					
+			}
+			else if(mes == null){
+				sa('Erro', 'Selecione o mês desejado', 'error', '', 'btn-danger');
+			}	
 		})
 	})
 </script>
 <div class='container'>
-	<form class='col-lg-3 col-md-3 col-sm-4 center' name='form_mes'>
+	<form class='col-lg-3 col-md-3 col-sm-4 center' id='selecter' method='post' action='<?php echo HOSPITAL.$_GET['action']; ?>_formulario?unst=1'>
 		<div class='form-group'>
 			<label for='select_setor'>Setor</label>
-			<select id='select_setor' class='form-control selectpicker' data-style="btn-info">
+			<select id='select_setor' name='id_setor' class='form-control selectpicker' data-style="btn-info">
 				<option disabled selected value=''>Selecione um setor</option>
 				<?php 
 				    foreach ($setores_hospital as $key => $setor_hospital):
@@ -51,7 +46,7 @@
 		</div>
 		<div class='form-group'>
 			<label for='select_mes'>Mês</label>
-			<select id='select_mes' class='form-control selectpicker' data-style="btn-info">
+			<select id='select_mes' name='id_mês' class='form-control selectpicker' data-style="btn-info">
 				<option disabled selected value=''>Selecione um mês</option>
 				<?php 
 				    foreach ($meses as $key => $mes):
@@ -64,5 +59,6 @@
 				?>
 			</select>
 		</div>
+		<button id='enviar' type='button' class='btn btn-primary'>Selecionar</button>
 	</form>
 </div>
