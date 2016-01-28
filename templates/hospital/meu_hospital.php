819ecc32fca0ff26ecd5ecd061dec46f321f08b3
Rodrigo Castro <rodrigondec@gmail.com>
2016-01-28 16:31:19 -0300

    adicionado 1 gráfico para cada indicador. Adicionado 1 grafico pra todos os indicadores

--- templates/hospital/meu_hospital.php
+++ templates/hospital/meu_hospital.php
@@ -2,24 +2,26 @@
 	<h2>Meu Hospital</h2>
 	<hr />
 </div>
+<?php 
+    $indicadores = select_many('id, nome', 'indicador'); 
+    echo "<script type='text/javascript'>var num_indicadores = ".count($indicadores).";</script>"
+?>
 <script type="text/javascript">
 	// Load the Visualization API and the piechart package.
 	google.load('visualization', '1.0', {'packages':['corechart']});
 
 	// Set a callback to run when the Google Visualization API is loaded.
-	google.setOnLoadCallback(drawChart);
+	google.setOnLoadCallback(drawCharts);
 
-	// Callback that creates and populates a data table,
-	// instantiates the pie chart, passes in the data and
-	// draws it.
-	function drawChart() {
+
+	function drawChart(indicador){
 		// Create the data table.
 		var array_data = new Array(['Mês', 'Limite superior', 'Nota', 'Limite inferior']);
 
 		$.ajax({
 		    type: "GET",
 		    url: 'http://localhost/smbps/templates/hospital/get_data_grafico.php',
-		    data: {id_setor: 1},
+		    data: {id_setor: 1, id_indicador: indicador},
 		    dataType: "jsonp",
 		    success: function(data){
 		    	for(var i = data.length - 1; i >= 0; i--) {
@@ -41,12 +43,57 @@
 		        };
 
 				// Instantiate and draw our chart, passing in some options.
-				var chart = new google.visualization.LineChart(document.getElementById('grafico'));
+				var chart = new google.visualization.LineChart(document.getElementById('grafico_'+indicador));
 				chart.draw(data, options);
 			}
-		});
+		})
 	}
+
+
+	// Callback that creates and populates a data table,
+	// instantiates the pie chart, passes in the data and
+	// draws it.
+	function drawCharts(){
+		drawChart(0);
+	}
+
+	$(window).resize(function(){
+		drawCharts();
+	})
 </script>
-<div class='container col-lg-6 center'>
-	<div id='grafico'><div>
+<!-- <div class='container col-lg-6 center'>
+	
+</div> -->
+<div class='container'>
+	<!-- MENU DE SELEÇÃO -->
+	<div class="list-group col-lg-2 col-md-2 col-sm-2 col-xs-3">
+		<a id='nav_indicador_0' class="list-group-item active" onclick='show_hide_indicador(0); drawChart(0);' href="#">
+			Geral
+		</a>
+	<?php 
+	    foreach ($indicadores as $num => $indicador):
+	?>
+		<a id='nav_indicador_<?php echo $indicador['id']; ?>' class="list-group-item" <?php echo "onclick='show_hide_indicador(".$indicador['id']."); drawChart(".$indicador['id'].");'" ?> href="#">
+			<?php echo $indicador['nome']; ?>
+		</a>
+	<?php 
+	    endforeach;
+	?>
+	</div>
+	<!-- INDICADORES -->
+	<div id='indicador_0' class='col-lg-6 col-md-6 col-sm-5 col-xs-6 center'>
+		<h3 class='text-center'>Dados gerais</h3>
+		<div id='grafico_0'></div>
+	</div>
+	<?php 
+	    foreach ($indicadores as $num => $indicador):
+	?>
+	
+	<div id='indicador_<?php echo $indicador['id']; ?>' class='col-lg-6 col-md-6 col-sm-5 col-xs-6 center hidden'>
+		<h3 class='text-center'><?php echo $indicador['nome']; ?></h3>
+		<div id='grafico_<?php echo $indicador['id']; ?>'></div>
+	</div>
+	<?php 
+		endforeach;
+	?>	
 </div>
